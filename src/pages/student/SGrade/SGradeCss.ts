eca7369a858f9c7e5c1cad94415fe0c1e58b32fc
choi <bcaca0831@gmail.com>
2023-03-27 00:06:51 +0900

    그래프 테스트

--- src/pages/student/SGrade/SGradeCss.ts
+++ src/pages/student/SGrade/SGradeCss.ts
@@ -25,6 +25,41 @@ const SGradeCss = styled.div`
             height: 320px;
             border-radius: 15px;
             background: #fff;
+            .newTest {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                .donutGraph {
+                    position: relative;
+                    width: 50%;
+
+                    .aa {
+                        position: absolute;
+                        top: 50%;
+                        left: 50%;
+                    }
+                }
+                .gradeList {
+                    position: relative;
+                    display: block;
+                    width: 50%;
+
+                    .gradeLR::before {
+                        position: absolute;
+                        content: "";
+                        width: 5px;
+                        height: 5px;
+                        border-radius: 50%;
+                        background: #000;
+                    }
+                    .gradeLR {
+                        display: flex;
+                        justify-content: space-between;
+                        padding-bottom: 10px;
+                        margin-left: 30px;
+                    }
+                }
+            }
         }
         .monthT {
             padding: 30px;
@@ -33,6 +68,13 @@ const SGradeCss = styled.div`
             border-radius: 15px;
             background: #fff;
         }
+    }
+    .analysis {
+        margin-top: 30px;
+        padding: 30px;
+        width: calc(520px * 2 + 30px);
+        border-radius: 15px;
+        background: #fff;
     }
 `;
 
